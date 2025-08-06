135da928be05e5100ed688e9b70d0f802e9d49a8
Fernando <vluisfernando62@gmail.com>
2025-08-05 20:20:02 -0400

    feat(tarea-1): configurar express, establecer vistas y agregar controlador para la agenda

--- tareas/1-crud-agenda/src/app.js
+++ tareas/1-crud-agenda/src/app.js
@@ -1,3 +1,21 @@
-const db = require("./config/database");
+const express = require("express");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const AgendaController = require("./controllers/AgendaController");
 
-console.log(db.getConnection());
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set("layout", "layout");
+
+app.get("/", AgendaController.index);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
